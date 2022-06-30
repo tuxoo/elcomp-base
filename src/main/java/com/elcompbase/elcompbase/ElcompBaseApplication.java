6e6dcdb66baefa6752a02f2b4939db2c3e3f0663
waccess.krivtsov <O;O$'LMMRt7^GDjH1M,=>
2022-06-30 17:28:22 +0300

    ElcompBase: init commit

---
+++ src/main/java/com/elcompbase/elcompbase/ElcompBaseApplication.java
@@ -0,0 +1,13 @@
+package com.elcompbase.elcompbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElcompBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElcompBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elcompbase/elcompbase/ElcompBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elcompbase.elcompbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElcompBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
